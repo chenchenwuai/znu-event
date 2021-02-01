14b0f651b4988746e1c260f826782de7ea8584aa
chenwuai <674939055@qq.com>
2021-02-01 14:29:05 +0800

    🐳chore: 修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -196,18 +196,5 @@ module.exports = {
       objectsInObjects: false
     }],
     'array-bracket-spacing': [2, 'never']
-
-    // vue
-    // "vue/max-attributes-per-line": [2, {
-    //   "singleline": 10,
-    //   "multiline": {
-    //     "max": 1,
-    //     "allowFirstLine": false
-    //   }
-    // }],
-    // "vue/singleline-html-element-content-newline": "off",
-    // "vue/multiline-html-element-content-newline": "off",
-    // "vue/name-property-casing": ["error", "PascalCase"],
-    // "vue/no-v-html": "off",
   }
 }
